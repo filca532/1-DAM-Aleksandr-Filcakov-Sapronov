354e0ea09d8fdefa34fd3d9ad21cd59052504a02
filca532 <alexfilcakov@gmail.com>
2023-11-22 09:28:55 +0100

    ED

--- ED/src/Exercici2.java
+++ ED/src/Exercici2.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Exercici2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N;
+        int n;
         System.out.print("Introduzca valor de N: ");
-        N = sc.nextInt(); //
+        n = sc.nextInt(); //
 
-        System.out.println("Primera cifra de " + N + " -> " + (N / 100));
-        System.out.println("Cifra central de " + N + " -> " + (N % 10));
-        System.out.println("Última cifra de " + N + " -> " + (N / 1));
+        System.out.println("Primera cifra de " + n + " -> " + (n / 100));
+        System.out.println("Cifra central de " + n + " -> " + (((n % 100) - (n % 10))) / 10);
+        System.out.println("Última cifra de " + n + " -> " + (n % 10));
     }
 
 }
