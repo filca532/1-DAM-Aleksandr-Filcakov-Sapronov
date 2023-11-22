f2b6f7c3af4249b3e116db1298d4cabc0c527d94
filca532 <alexfilcakov@gmail.com>
2023-11-22 09:33:58 +0100

    ED

---
+++ ED/src/Exercici3.java
@@ -0,0 +1,19 @@
+public class Exercici3 {
+    public static void main(String[] args) {
+        int i, suma = 0, n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        n = sc.nextInt();
+        for (i = 1; i > n; i++) {
+            if (n % i == 0) {
+                suma = +i;
+            }
+        }
+        if (suma == n) {
+            System.out.println("Perfecto");
+        } else {
+            System.out.println("No es perfecto");
+        }
+
+    }
+}
