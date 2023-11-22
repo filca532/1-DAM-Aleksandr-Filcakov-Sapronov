053b2a10bb880357522b7130ce81e4f8ff945758
filca532 <alexfilcakov@gmail.com>
2023-11-22 09:01:56 +0100

    ED

--- ED/src/JavaApplication10.java
+++ ED/src/JavaApplication10.java
@@ -9,6 +9,6 @@ public class JavaApplication10 {
         radio = sc.nextDouble();
         longitud = 2 * Math.PI * radio;
         area = Math.PI * Math.pow(radio, Math.PI);
-        System.out.println("Longitud de la circunferencia -> " + longitud);
+        System.out.println("Longitud de la circunferencia ytyd6tlodltoklts5olsoklsltsxtskltyo -> " + longitud);
         System.out.println("Area de la circunferencia -> " + area);
     }}
