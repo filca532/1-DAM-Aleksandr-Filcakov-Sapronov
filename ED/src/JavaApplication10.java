924c824bec59d92bfdeb3ae3265ce1580314bf76
filca532 <alexfilcakov@gmail.com>
2023-11-22 08:20:45 +0100

    ED

---
+++ ED/src/JavaApplication10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class JavaApplication10 {
+    public static void main(String[] args) {
+        Scanner sc;
+        sc = new Scanner(System.in);
+        double radio, longitud, area;
+        System.out.println("Introduce radio de la circunferencia:");
+        radio = sc.nextDouble();
+        longitud = 2 * Math.PI * radio;
+        area = Math.PI * Math.pow(radio, Math.PI);
+        System.out.println("Longitud de la circunferencia -> " + longitud);
+        System.out.println("Area de la circunferencia -> " + area);
+    }}
