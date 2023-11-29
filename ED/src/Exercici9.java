8846db37926175fd4ff66e7fb4b3c7c1cc630a57
filca532 <alexfilcakov@gmail.com>
2023-11-29 09:45:18 +0100

    ED

--- ED/src/Exercici9.java
+++ ED/src/Exercici9.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Exercici9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N, aux, cifra, numeroCifras = 0;
+        double suma;
+        do {
+            System.out.print("Introduce número entero positivo: ");
+            N = sc.nextInt();
+            if (N <= 0) {
+                System.out.println("Error. Debe ser un número positivo");
+            }
+        } while (N <= 0);
+        aux = N;
+        while (aux != 0) {
+            numeroCifras++;
+            aux = aux / 10; //Poner un %
+        }
+        suma = 0;
+        aux = N;
+        while (aux != 0) {
+            cifra = aux % 10;
+            suma += Math.pow(cifra, numeroCifras);
+            aux = aux / 10;
+        }
+        if (suma == N) {
+            System.out.println("Es narcisista");
+        } else {
+            System.out.println("No es narcisista");
+        }
+    }
 }
+
