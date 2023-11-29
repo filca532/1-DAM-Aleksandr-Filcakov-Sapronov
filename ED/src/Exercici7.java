4ad10a4dabd75cf57ccb50a87915ff9aed69b144
filca532 <alexfilcakov@gmail.com>
2023-11-29 08:49:33 +0100

    ED

--- ED/src/Exercici7.java
+++ ED/src/Exercici7.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class Exercici7 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numfiguras;
+        do {
+            System.out.print("¿Cuántas figuras quieres?");
+            numfiguras = entrada.nextInt();
+        } while (numfiguras < 2);
+        int nfiguras = numfiguras;
+        int numlinea;
+        do {
+            System.out.print("¿Cuántos asteriscos quieres?");
+            numlinea = entrada.nextInt();
+        } while (numlinea < 2);
+        int numespacios = 0;
+        int posasterisco;
+        int nespacios;
+        while (numlinea > 0) {
+            numfiguras = nfiguras;
+            posasterisco = numlinea;
+            nespacios = numespacios;
+            while (numfiguras > 0) {
+                while (posasterisco > 0) {
+                    System.out.print("*");
+                }
+                while (nespacios-- > 0) {
+                    System.out.print(" ");
+                }
+                if (numfiguras > 1) {
+                    System.out.print("||");
+                }
+                numfiguras = numfiguras - 1;
+            }
+            System.out.println("");
+            numlinea = numlinea - 1;
+            numespacios = numespacios + 1;
+        }
+    }
 }
